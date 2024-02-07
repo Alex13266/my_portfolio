a00a2ed50ebaa9e1d26bbe711e704e867d97c58e
Alex13266 <79878120+Alex13266@users.noreply.github.com>
2024-02-08 00:02:01 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,10 +6,10 @@ const nodemailer = require("nodemailer");
 // const router = express.Router();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 const CONNECTION_URI =
-  "mongodb+srv://admin:<password>@cluster0.2b1rw41.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://admin:Jnpug9GI4Vpph38U@cluster0.2b1rw41.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(CONNECTION_URI);
 
 const cardSchema = new mongoose.Schema({
