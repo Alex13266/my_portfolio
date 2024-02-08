6611d97a7fa238e043971d9bf3565f727d2afe93
Alex13266 <79878120+Alex13266@users.noreply.github.com>
2024-02-09 00:32:52 +0200

    update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const nodemailer = require("nodemailer");
 // const router = express.Router();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 const CONNECTION_URI =
   "mongodb+srv://admin:Jnpug9GI4Vpph38U@cluster0.2b1rw41.mongodb.net/?retryWrites=true&w=majority";
@@ -21,7 +21,13 @@ const cardSchema = new mongoose.Schema({
 
 const Card = mongoose.model("Card", cardSchema);
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://my-portfolio-w8ht.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // - - - - - - - - - - - - -
