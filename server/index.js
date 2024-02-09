34ed1f45f6a6cd6b2a5be749322f3030855dea8b
Alex13266 <aleksivanov845@gmail.com>
2024-02-10 00:35:49 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ const cardSchema = new mongoose.Schema({
 const Card = mongoose.model("Card", cardSchema);
 
 const corsConfig = {
-  origin: "*",
+  origin: "https://my-portfolio-teal-eight-65.vercel.app/",
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
 };
